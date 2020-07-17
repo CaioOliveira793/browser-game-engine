dd90482e093ef491207242759a63967987176172
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-07-17 11:46:36 -0300

    feat(renderer): query uniform locations on shader creation
    when a new shader is created, all uniforms is queries and stored in a Map

--- src/Renderer/Shader.ts
+++ src/Renderer/Shader.ts
@@ -6,123 +6,140 @@ class Shader {
 
 	private id: WebGLProgram = 0;
 
-	private name: string;
+	private uniforms: Map<string, { type: number, location: WebGLUniformLocation }>
 
-	constructor(name: string, vertexSrc: string, fragmentSrc: string) {
-		this.name = name;
-
+	constructor(vertexSrc: string, fragmentSrc: string) {
 		this.compile(new Map([
 			[Shader.ctx.VERTEX_SHADER, vertexSrc],
 			[Shader.ctx.FRAGMENT_SHADER, fragmentSrc]
 		]));
-	}
 
-	public getName = (): string => this.name;
+		this.uniforms = new Map();
+		const uniformCount = Shader.ctx.getProgramParameter(this.id, Shader.ctx.ACTIVE_UNIFORMS);
+
+		for (let i = 0; i < uniformCount; i++) {
+			const uniform = Shader.ctx.getActiveUniform(this.id, i) as WebGLActiveInfo;
+
+			this.uniforms.set(uniform.name,
+				{
+					type: uniform.type,
+					location: Shader.ctx.getUniformLocation(this.id, uniform.name) as WebGLUniformLocation
+				});
+		}
+	}
 
 	public bind = (): void => { Shader.ctx.useProgram(this.id); }
 	public delete = (): void => { Shader.ctx.deleteProgram(this.id); }
 
 	public uploadUniformFloat = (name: string, f1: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform1f(location, f1);
+		Shader.ctx.uniform1f(this.uniforms.get(name)?.location as WebGLUniformLocation, f1);
 	}
 	public uploadUniformFloat2 = (name: string, f1: number, f2: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2f(location, f1, f2);
+		Shader.ctx.uniform2f(this.uniforms.get(name)?.location as WebGLUniformLocation, f1, f2);
 	}
 	public uploadUniformFloat3 = (name: string, f1: number, f2: number, f3: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3f(location, f1, f2, f3);
+		Shader.ctx.uniform3f(this.uniforms.get(name)?.location as WebGLUniformLocation, f1, f2, f3);
 	}
 	public uploadUniformFloat4 = (name: string, f1: number, f2: number, f3: number, f4: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4f(location, f1, f2, f3, f4);
+		Shader.ctx.uniform4f(this.uniforms.get(name)?.location as WebGLUniformLocation, f1, f2, f3, f4);
 	}
 
 	public uploadUniformVec2 = (name: string, vec2: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2fv(location, vec2);
+		Shader.ctx.uniform2fv(this.uniforms.get(name)?.location as WebGLUniformLocation, vec2);
 	}
 	public uploadUniformVec3 = (name: string, vec3: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3fv(location, vec3);
+		Shader.ctx.uniform3fv(this.uniforms.get(name)?.location as WebGLUniformLocation, vec3);
 	}
 	public uploadUniformVec4 = (name: string, vec4: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4fv(location, vec4);
+		Shader.ctx.uniform4fv(this.uniforms.get(name)?.location as WebGLUniformLocation, vec4);
 	}
 
 	public uploadUniformInt = (name: string, i1: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform1i(location, i1);
+		Shader.ctx.uniform1i(this.uniforms.get(name)?.location as WebGLUniformLocation, i1);
 	}
 	public uploadUniformInt2 = (name: string, i1: number, i2: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2i(location, i1, i2);
+		Shader.ctx.uniform2i(this.uniforms.get(name)?.location as WebGLUniformLocation, i1, i2);
 	}
 	public uploadUniformInt3 = (name: string, i1: number, i2: number, i3: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3i(location, i1, i2, i3);
+		Shader.ctx.uniform3i(this.uniforms.get(name)?.location as WebGLUniformLocation, i1, i2, i3);
 	}
 	public uploadUniformInt4 = (name: string, i1: number, i2: number, i3: number, i4: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4i(location, i1, i2, i3, i4);
+		Shader.ctx.uniform4i(this.uniforms.get(name)?.location as WebGLUniformLocation, i1, i2, i3, i4);
 	}
 
 	public uploadUniformIVec2 = (name: string, ivec2: Int32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2iv(location, ivec2);
+		Shader.ctx.uniform2iv(this.uniforms.get(name)?.location as WebGLUniformLocation, ivec2);
 	}
 	public uploadUniformIVec3 = (name: string, ivec3: Int32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3iv(location, ivec3);
+		Shader.ctx.uniform3iv(this.uniforms.get(name)?.location as WebGLUniformLocation, ivec3);
 	}
 	public uploadUniformIVec4 = (name: string, ivec4: Int32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4iv(location, ivec4);
+		Shader.ctx.uniform4iv(this.uniforms.get(name)?.location as WebGLUniformLocation, ivec4);
 	}
 
 	public uploadUniformUInt = (name: string, ui1: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform1ui(location, ui1);
+		Shader.ctx.uniform1ui(this.uniforms.get(name)?.location as WebGLUniformLocation, ui1);
 	}
 	public uploadUniformUInt2 = (name: string, ui1: number, ui2: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2ui(location, ui1, ui2);
+		Shader.ctx.uniform2ui(this.uniforms.get(name)?.location as WebGLUniformLocation, ui1, ui2);
 	}
 	public uploadUniformUInt3 = (name: string, ui1: number, ui2: number, ui3: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3ui(location, ui1, ui2, ui3);
+		Shader.ctx.uniform3ui(this.uniforms.get(name)?.location as WebGLUniformLocation, ui1, ui2, ui3);
 	}
 	public uploadUniformUInt4 = (name: string, ui1: number, ui2: number, ui3: number, ui4: number): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4ui(location, ui1, ui2, ui3, ui4);
+		Shader.ctx.uniform4ui(this.uniforms.get(name)?.location as WebGLUniformLocation, ui1, ui2, ui3, ui4);
 	}
 
 	public uploadUniformUVec2 = (name: string, uvec2: Uint32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform2uiv(location, uvec2);
+		Shader.ctx.uniform2uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, uvec2);
 	}
 	public uploadUniformUVec3 = (name: string, uvec3: Uint32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform3uiv(location, uvec3);
+		Shader.ctx.uniform3uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, uvec3);
 	}
 	public uploadUniformUVec4 = (name: string, uvec4: Uint32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniform4uiv(location, uvec4);
+		Shader.ctx.uniform4uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, uvec4);
+	}
+
+	public uploadUniformBool = (name: string, b1: boolean): void => {
+		Shader.ctx.uniform1ui(this.uniforms.get(name)?.location as WebGLUniformLocation, b1 as unknown as number);
+	}
+	public uploadUniformBool2 = (name: string, b1: boolean, b2: boolean): void => {
+		Shader.ctx.uniform2ui(this.uniforms.get(name)?.location as WebGLUniformLocation,
+			b1 as unknown as number,
+			b2 as unknown as number);
+	}
+	public uploadUniformBool3 = (name: string, b1: boolean, b2: boolean, b3: boolean): void => {
+		Shader.ctx.uniform3ui(this.uniforms.get(name)?.location as WebGLUniformLocation,
+			b1 as unknown as number,
+			b2 as unknown as number,
+			b3 as unknown as number);
+	}
+	public uploadUniformBool4 = (name: string, b1: boolean, b2: boolean, b3: boolean, b4: boolean): void => {
+		Shader.ctx.uniform4ui(this.uniforms.get(name)?.location as WebGLUniformLocation,
+			b1 as unknown as number,
+			b2 as unknown as number,
+			b3 as unknown as number,
+			b4 as unknown as number);
+	}
+
+	public uploadUniformBVec2 = (name: string, bvec2: Uint32Array | boolean[]): void => {
+		Shader.ctx.uniform2uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, bvec2 as unknown as number[]);
+	}
+	public uploadUniformBVec3 = (name: string, bvec3: Uint32Array | boolean[]): void => {
+		Shader.ctx.uniform3uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, bvec3 as unknown as number[]);
+	}
+	public uploadUniformBVec4 = (name: string, bvec4: Uint32Array | boolean[]): void => {
+		Shader.ctx.uniform4uiv(this.uniforms.get(name)?.location as WebGLUniformLocation, bvec4 as unknown as number[]);
 	}
 
 	public uploadUniformMat2 = (name: string, mat2: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniformMatrix2fv(location, false, mat2);
+		Shader.ctx.uniformMatrix2fv(this.uniforms.get(name)?.location as WebGLUniformLocation, false, mat2);
 	}
 	public uploadUniformMat3 = (name: string, mat3: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniformMatrix3fv(location, false, mat3);
+		Shader.ctx.uniformMatrix3fv(this.uniforms.get(name)?.location as WebGLUniformLocation, false, mat3);
 	}
 	public uploadUniformMat4 = (name: string, mat4: Float32Array | number[]): void => {
-		const location = Shader.ctx.getUniformLocation(this.id, name);
-		Shader.ctx.uniformMatrix4fv(location, false, mat4);
+		Shader.ctx.uniformMatrix4fv(this.uniforms.get(name)?.location as WebGLUniformLocation, false, mat4);
 	}
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
 		}]
 	},
 	devServer: {
+		host: '0.0.0.0',
 		port: 9000,
 		compress: true,
 		hot: true,
