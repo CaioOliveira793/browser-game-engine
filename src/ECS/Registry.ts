e20ffe993e66c2b489e86e475ebbdf50c305ef92
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-08-15 07:59:22 -0300

    feat(ecs): able to get intersection of entities in component lists

--- src/ECS/Registry.ts
+++ src/ECS/Registry.ts
@@ -1,7 +1,9 @@
 type Entity = number;
 
 interface Storage {
-	[key: string]: Map<number, Component<unknown>>;
+	[key: string]: {
+		data: Map<number, Component<unknown>>;
+	}
 }
 
 interface Component<T> {
@@ -13,7 +15,9 @@ class Registry {
 	constructor(componentConstructors: (new (...args: unknown[]) => unknown)[]) {
 		this.storages = {};
 		for (const component of componentConstructors)
-			this.storages[component.name] = new Map();
+			this.storages[component.name] = {
+				data: new Map()
+			};
 		this.entityIdIncrement = 0;
 		this.entityComponentCounter = [];
 	}
@@ -22,49 +26,81 @@ class Registry {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public create = (): Entity => {
-		this.entityComponentCounter[this.entityIdIncrement] = 0;
+		this.entityComponentCounter[this.entityIdIncrement] = [];
 		return this.entityIdIncrement++;
 	}
 
 	public destroy = (entity: Entity): void => {
-		let componentCount = this.entityComponentCounter[entity];
-		for (const constructorsName in this.storages) {
-			if (this.storages[constructorsName].delete(entity)) componentCount--;
-			if (componentCount === 0) break;
+		for (const constructorsName in this.entityComponentCounter[entity]) {
+			this.storages[constructorsName].data.delete(entity);
 		}
 
-		this.entityComponentCounter[entity] = componentCount;
+		this.entityComponentCounter[entity] = [];
 	}
 
-	public getComponentCount = (entity: Entity): number => this.entityComponentCounter[entity];
+	public getComponentCount = (entity: Entity): number => this.entityComponentCounter[entity].length;
 
 	// component /////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public insert = <T>(entity: Entity, componentConstructor: new (...args: unknown[]) => T, ...args: unknown[]): Component<T> => {
 		const component = { value: new componentConstructor(...args) };
-		this.storages[componentConstructor.name].set(entity, component);
-		this.entityComponentCounter[entity]++;
+		this.storages[componentConstructor.name].data.set(entity, component);
+		this.entityComponentCounter[entity].push(componentConstructor.name);
 		return component;
 	}
 
-	public get = <T>(entity: Entity, componentConstructor: new (...args: unknown[]) => T): Component<T> | undefined => {
-		if (this.entityComponentCounter[entity] === 0)
+	public getComponents = <T>(entity: Entity, componentConstructor: (new (...args: unknown[]) => T)[]): (Component<T> | undefined)[] => {
+		if (this.entityComponentCounter[entity].length === 0)
 			throw new Error('entity does not have any component to retrieve');
 
-		return this.storages[componentConstructor.name].get(entity) as Component<T>;
+		const componentsList = [];
+		for (const constructor of componentConstructor) {
+			componentsList.push(this.storages[constructor.name].data.get(entity) as Component<T>);
+		}
+		return componentsList;
+	}
+
+	public getIntersection = (componentConstructors: (new (...args: unknown[]) => unknown)[]): Set<Entity> => {
+		const sortedConstructorNames = componentConstructors
+			.map(constructors => constructors.name)
+			.sort((first, next) => this.storages[first].data.size - this.storages[next].data.size);
+
+		const smallerComponentListName = sortedConstructorNames.shift() as string;
+		if (sortedConstructorNames.length === 0)
+			return new Set(this.storages[smallerComponentListName].data.keys());
+
+		const intersection = new Set<Entity>();
+
+		let components = 0;
+		for (const entity of this.storages[smallerComponentListName].data.keys()) {
+			components = 0;
+			for (const name of sortedConstructorNames) {
+				if (this.entityComponentCounter[entity].indexOf(name) === -1) {
+					break;
+				}
+				components++;
+			}
+			if (components === sortedConstructorNames.length)
+				intersection.add(entity);
+		}
+
+		return intersection;
 	}
 
 	public remove = (entity: Entity, componentConstructor: new (...args: unknown[]) => unknown): void => {
-		if (this.entityComponentCounter[entity] === 0)
+		if (this.entityComponentCounter[entity].length === 0)
 			throw new Error('entity does not have any component to remove');
 
-		this.storages[componentConstructor.name].delete(entity);
-		this.entityComponentCounter[entity]--;
+		this.storages[componentConstructor.name].data.delete(entity);
+
+		const index = this.entityComponentCounter[entity].indexOf(componentConstructor.name);
+		if (index !== -1)
+			this.entityComponentCounter[entity].splice(index, 1);
 	}
 
 	public clear = (componentConstructor: new (...args: unknown[]) => unknown): void => {
-		this.storages[componentConstructor.name].clear();
+		this.storages[componentConstructor.name].data.clear();
 	}
 
 	// registry //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -72,13 +108,13 @@ class Registry {
 
 	public clearAll = (): void => {
 		for (const constructorsName in this.storages)
-			this.storages[constructorsName].clear();
+			this.storages[constructorsName].data.clear();
 	}
 
 
 
 	private readonly storages: Storage;
-	private readonly entityComponentCounter: number[];
+	private readonly entityComponentCounter: string[][];
 
 	private entityIdIncrement: number;
 }
